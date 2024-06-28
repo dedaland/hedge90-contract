229a30c47057f7672a60dff1d5c3f59d4f3b7735
salva-imm <mehdi73ee@gmail.com>
2024-06-28 04:19:51 +0330

    return all contract addresses

--- script/DeployHedge90.s.sol
+++ script/DeployHedge90.s.sol
@@ -8,7 +8,7 @@ import { DedaCoin } from "../src/DummyDeDa.sol";
 import { TokenPriceManager } from "../src/TokenPriceManager.sol";
 
 contract DeployHedge90 is Script {
-    function run() external returns(TokenSale) {
+    function run() external returns(TokenSale, TokenPriceManager, MyUSDT, DedaCoin) {
         address DummyACC0 = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
         address DummyACC1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
         address DummyACC2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
@@ -31,6 +31,6 @@ contract DeployHedge90 is Script {
             address(tokenpricemanager)
         );
         vm.stopBroadcast();
-        return tokensale;
+        return (tokensale, tokenpricemanager, usdt, dedacoin);
     }
 }
